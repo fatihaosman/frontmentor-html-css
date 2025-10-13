08c22535a3cf02e55fcde700805fac20d591c923
fatihaosman <fatihaosman17@gmail.com>
2025-10-13 11:51:02 +0300

    strted on js part

---
+++ docs/newsletter-sign-up-with-success-message-main/script.js
@@ -0,0 +1,9 @@
+ var button =document.querySelector("button");
+  var form = document.querySelector("form");
+  var newsletter = document.querySelector(".newsletter");
+  var errorMessage = document.querySelector(".error-message");
+  var input = document.querySelector("input");
+
+  button.addEventListener("click", function(event){
+    
+  })
